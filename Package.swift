e2da3ec9ba3a27143fc414375a11300b5ae1177b
Luther Stanton <luther.stanton@gmail.com>
2025-06-22 21:11:11 -0400

    adds initial package structure

---
+++ Package.swift
@@ -0,0 +1,24 @@
+// swift-tools-version: 6.1
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "MimNetBridge",
+    products: [
+        // Products define the executables and libraries a package produces, making them visible to other packages.
+        .library(
+            name: "MimNetBridge",
+            targets: ["MimNetBridge"]),
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package, defining a module or a test suite.
+        // Targets can depend on other targets in this package and products from dependencies.
+        .target(
+            name: "MimNetBridge"),
+        .testTarget(
+            name: "MimNetBridgeTests",
+            dependencies: ["MimNetBridge"]
+        ),
+    ]
+)
